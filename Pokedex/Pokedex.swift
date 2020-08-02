f2b77f487d133c3c7040668168080b4416701ee2
Yusufmasud1234 <63847075+Yusufmasud1234@users.noreply.github.com>
2020-08-02 19:28:07 +0530

    Add files via upload

---
+++ Pokedex/Pokedex.swift
@@ -0,0 +1,16 @@
+//
+//  Pokedex.swift
+//  Pokedex
+//
+//  Created by Edd Sansome on 26/01/2020.
+//  Copyright © 2020 CS50. All rights reserved.
+//
+
+import Foundation
+
+struct Pokedex {
+    
+    var caught = [String: Bool]()
+   
+
+    }
